54855a9ab072dbc9cca9a9d9365186ab2b5302f6
Mohammad Ghaemi <seyedmohammadghaem@gmail.com>
2019-08-14 14:52:13 -0400

    Init

---
+++ ShowItemGot/ShowItemGot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Modding;
+using JetBrains.Annotations;
+using ModCommon;
+using MonoMod.RuntimeDetour;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using UObject = UnityEngine.Object;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ShowItemGot
+{
+    [UsedImplicitly]
+    public class ShowItemGot : Mod, ITogglableMod
+    {
+        public static Dictionary<string, GameObject> preloadedGO = new Dictionary<string, GameObject>();
+
+        public static ShowItemGot Instance;
+
+        public static readonly List<Sprite> SPRITES = new List<Sprite>();
+
+        public override string GetVersion()
+        {
+            return "0.0.0.0";
+        }
+
+        public override List<(string, string)> GetPreloadNames()
+        {
+            return new List<(string, string)>
+            {
+                ("Fungus1_Slug","Giant Slug NPC/Shiny Item")
+            };
+        }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            preloadedGO.Add("item", preloadedObjects["Fungus1_Slug"]["Giant Slug NPC/Shiny Item"]);
+            Instance = this;
+            Log("Initalizing.");
+
+            Unload();
+            ModHooks.Instance.AfterSavegameLoadHook += AfterSaveGameLoad;
+            ModHooks.Instance.NewGameHook += AddComponent;
+        }
+
+        private void AfterSaveGameLoad(SaveGameData data) => AddComponent();
+
+        private void AddComponent()
+        {
+            GameManager.instance.gameObject.AddComponent<ThisDoDaThing>();
+        }
+
+        public void Unload()
+        {
+            AudioListener.volume = 1f;
+            AudioListener.pause = false;
+            ModHooks.Instance.AfterSavegameLoadHook -= AfterSaveGameLoad;
+            ModHooks.Instance.NewGameHook -= AddComponent;
+
+            // ReSharper disable once Unity.NoNullPropogation
+            var x = GameManager.instance?.gameObject.GetComponent<ThisDoDaThing>();
+            if (x == null) return;
+            UObject.Destroy(x);
+        }
+    }
+}
---
+++ ShowItemGot/ThisDoDaThing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker.Actions;
+using Logger = Modding.Logger;
+using UnityEngine.SceneManagement;
+using UObject = UnityEngine.Object;
+using ModCommon.Util;
+using ModCommon;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using System.IO;
+using System;
+
+namespace ShowItemGot
+{
+    internal class ThisDoDaThing : MonoBehaviour
+    {
+        private const int CHARM_NUM = 1;
+
+        private void Start()
+        {
+            StartCoroutine(Testing());
+        }
+
+        IEnumerator Testing()
+        {
+            yield return new WaitWhile(() => HeroController.instance == null);
+            yield return new WaitForSeconds(10f); //For testing purposes
+            /*
+            If you want to stop the player from moving.
+            HeroController.instance.RelinquishControl();
+            HeroController.instance.StopAnimationControl();*/
+            var fsm = ShowItemGot.preloadedGO["item"].LocateMyFSM("Shiny Control");
+            yield return null;
+            fsm.GetAction<SetFsmInt>("Normal Msg", 1).setValue.Value = CHARM_NUM;
+            yield return null;
+            fsm.enabled = true;
+            fsm.SetState("Normal Msg");
+            PlayerData.instance.gotCharm_1 = true;
+            /*
+            Gives Control back.
+            HeroController.instance.StartAnimationControl();
+            HeroController.instance.RegainControl();*/
+        }
+
+        private void Log(object o)
+        {
+            Logger.Log("[Show] " + o);
+        }
+    }
+}
